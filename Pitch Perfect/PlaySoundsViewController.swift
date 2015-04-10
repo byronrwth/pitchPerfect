0af09c33992441344794ad617917715b475f686b
gchavez2@gmail.com <gchavez2@gmail.com>
2015-04-10 20:17:04 +0300

    Finish up to lesson 4a: RecordingAudio

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -16,7 +16,7 @@ class PlaySoundsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view. 
         
 //        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
 //            var filepathURL = NSURL.fileURLWithPath(filePath)
